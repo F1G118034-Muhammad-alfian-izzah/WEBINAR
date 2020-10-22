afcc96528504f20069c1cb92dee5e7a7447c7ade
F1G118034-Muhammad-alfian-izzah <alfianizzah19@gmail.com>
2020-10-22 23:22:55 +0800

    perubahan

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -7,9 +7,9 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>COBA COBA</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam eligendi quam sunt, optio eos distinctio sed cumque dicta totam voluptatem!</p>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
